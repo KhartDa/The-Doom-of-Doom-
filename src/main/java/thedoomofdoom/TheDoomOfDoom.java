a5e550871e34dd97cd2cc778b9600d5a26f3c3f7
KhartDa <khart.david10@gmail.com>
2025-10-21 11:34:47 -0700

    Partually Cursor Fix

--- src/main/java/thedoomofdoom/TheDoomOfDoom.java
+++ src/main/java/thedoomofdoom/TheDoomOfDoom.java
@@ -6,6 +6,7 @@ import com.jme3.math.ColorRGBA;
 import com.jme3.renderer.RenderManager;
 import com.jme3.scene.Geometry;
 import com.jme3.scene.shape.Box;
+import com.jme3.system.AppSettings;
 
 /**
  * This is the Main Class of your Game. It should boot up your game and do initial initialisation
@@ -14,13 +15,27 @@ import com.jme3.scene.shape.Box;
 public class TheDoomOfDoom extends SimpleApplication {
 
     public static void main(String[] args) {
+        
+        
         TheDoomOfDoom app = new TheDoomOfDoom();
         app.setShowSettings(false); //Settings dialog not supported on mac
+        // app.setResolution(1280, 720);
+        
         app.start();
     }
 
     @Override
     public void simpleInitApp() {
+
+        AppSettings settings = new AppSettings(false);
+        settings.setResizable(true);
+        settings.setResolution(1280, 720);
+
+        inputManager.deleteMapping(SimpleApplication.INPUT_MAPPING_EXIT);
+
+        flyCam.setDragToRotate(true); // Only rotate when dragging
+        inputManager.setCursorVisible(true); // Free the mouse
+
         Box b = new Box(1, 1, 1);
         Geometry geom = new Geometry("Box", b);
 
@@ -33,6 +48,13 @@ public class TheDoomOfDoom extends SimpleApplication {
 
     @Override
     public void simpleUpdate(float tpf) {
+        if (!inputManager.isCursorVisible()) {
+            flyCam.setDragToRotate(false);
+        }
+        else {
+            flyCam.setDragToRotate(true);
+        }
+
         //this method will be called every game tick and can be used to make updates
     }
 
